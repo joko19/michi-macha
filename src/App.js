bb10c0a43a71606e3c7334a8ecd71378d1859b41
joko19 <jokoslamet459@gmail.com>
2022-07-01 02:28:48 +0700

    utility

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className='bg-custom px-8 md:px-24 bg-custom-blue'>
+    <div className='bg-custom bg-custom-blue'>
       <header className='flex flex-row-reverse'>
         <div className='flex'>
           <img src='icons/discord.svg' alt='icon discord' />
@@ -10,21 +10,22 @@ function App() {
           <img src='icons/instagram.svg' alt='icon instagram' />
         </div>
       </header>
+      
       {/* michi macha */}
-      <section className=' text-center  '>
-        <h1 className='text-6xl text-custom-blue font-bubblegum text-stroke-bold'>Michi</h1>
-        <h1 className='text-6xl text-custom-blue font-bubblegum text-stroke-bold'>Macha</h1>
+      <section className=' text-center md:px-24 px-8 '>
+        <h1 className='text-7xl text-custom-blue font-bubblegum text-stroke-bold'>Michi</h1>
+        <h1 className='text-7xl text-custom-blue font-bubblegum text-stroke-bold'>Macha</h1>
         <p className='text-custom-gray text-2xl font-bubblegumSans text-lg md:py-12 md:px-36'>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
         </p>
         <h1 className='text-4xl text-custom-blue font-bubblegum text-stroke'>MINT SOON</h1>
       </section>
       {/* story */}
-      <section className='flex mt-12 gap-8'>
+      <section className='flex mt-12 gap-8 md:px-24 px-8 '>
         <div className='w-full'>
-          <h1 className='text-6xl text-custom-yellow font-bubblegum text-stroke-bold'>Story</h1>
+          <h1 className='text-7xl text-custom-yellow font-bubblegum text-stroke-bold'>Story</h1>
           <p className='text-custom-gray text-2xl font-bubblegumSans text-lg py-4 '>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
           </p>
         </div>
         <div className='flex md:w-1/2'>
@@ -32,8 +33,8 @@ function App() {
         </div>
       </section>
       {/*   my michis machas */}
-      <section className='mt-12'>
-        <h1 className='text-6xl text-custom-yellow font-bubblegum text-stroke-bold text-center'>My Michis Machas</h1>
+      <section className='mt-12 md:px-24 px-8 '>
+        <h1 className='text-7xl text-custom-yellow font-bubblegum text-stroke-bold text-center'>My Michis Machas</h1>
         <div className='flex flex-col md:flex-row'>
           <div className='w-full'>
             <img src='/img/cute.svg' alt='cute'  />
@@ -55,6 +56,26 @@ function App() {
             <p className='text-custom-gray text-2xl font-bubblegumSans'>
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
             </p>
+          </div>
+        </div>
+      </section>
+      {/* Utility */}
+      <section className='mt-12'>
+        <h1 className='text-7xl text-custom-yellow font-bubblegum text-stroke-bold text-center py-4'>Utility</h1>
+        <div className='flex flex-col md:-px-24 md:flex-row'>
+          <div className='w-full'>
+            <img src='/img/utility-1.svg' alt='utility 1' />
+            <img src='/img/utility-2.svg' alt='utility 2' />
+            <img src='/img/utility-3.svg' alt='utility 3' />
+          </div>
+          <div className='w-1/2 p-4'>
+            <h1 className='text-custom-red text-stroke font-bubblegum text-4xl'>Include</h1>
+            <ul className='text-custom-gray text-2xl font-bubblegumSans'>
+              <li className='list-disc'>amco laboris nisi ut aliquip ex ea commodo consequat. Duis aute laboris nisi ut  commodo consequat. </li>
+              <li className='list-disc'>amco laboris nisi ut aliquip ex ea commodo consequat. Duis aute laboris nisi ut  commodo consequat. </li>
+              <li className='list-disc'>amco laboris nisi ut aliquip ex ea commodo consequat. Duis aute laboris nisi ut  commodo consequat. </li>
+              <li className='list-disc'>amco laboris nisi ut aliquip ex ea commodo consequat. Duis aute laboris nisi ut  commodo consequat. </li>
+            </ul>
           </div>
         </div>
       </section>
