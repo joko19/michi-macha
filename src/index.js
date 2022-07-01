67f690b49c4bf18f96189ea90cd6b9bc603181b0
joko19 <jokoslamet459@gmail.com>
2022-07-02 00:58:59 +0700

    multiple language

--- src/index.js
+++ src/index.js
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { sendToVercelAnalytics } from './vitals';
+import i18next from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import HttpApi from 'i18next-http-backend'
+import LanguageDetector from 'i18next-browser-languagedetector'
+
+i18next
+  .use(HttpApi)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    supportedLngs: ['id', 'en'],
+    fallbackLng: 'en',
+    debug: false,
+    // Options for language detector
+    detection: {
+      order: ['path', 'cookie', 'htmlTag'],
+      caches: ['cookie'],
+    },
+    // react: { useSuspense: false },
+    backend: {
+      loadPath: '/locale/{{lng}}/translation.json',
+    },
+  })
+
+  const loadingMarkup = (
+    <div className="py-4 text-center">
+      <h3>Loading..</h3>
+    </div>
+  )  
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Suspense fallback={loadingMarkup}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Suspense>,
   document.getElementById('root')
 );
 
