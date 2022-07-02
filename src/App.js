e17e9aa5061c1e566e02f54f843debf686373d56
joko19 <jokoslamet459@gmail.com>
2022-07-03 03:51:58 +0700

    translation

--- src/App.js
+++ src/App.js
@@ -57,17 +57,14 @@ function App() {
         <p className='text-custom-gray text-2xl font-bubblegumSans md:py-12 md:px-36'>
           {t('description')}
         </p>
-        <h1 className='text-4xl text-custom-blue font-bubblegum text-stroke'>{t('mint_soon')}
-        </h1>
+        <h1 className='text-4xl text-custom-blue font-bubblegum text-stroke'>{t('mint_soon')}</h1>
       </section>
 
       {/* story */}
       <section className='flex flex-col md:flex-row mt-24 gap-8 md:px-24 px-8 '>
         <div className='w-full'>
           <h1 className='text-7xl text-custom-yellow font-bubblegum text-stroke-bold'>{t('story')}</h1>
-          <p className='text-custom-gray text-2xl font-bubblegumSans py-4 '>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
-          </p>
+          <p className='text-custom-gray text-2xl font-bubblegumSans py-4 '>{t('story_desc')}</p>
         </div>
         <div className='flex md:w-1/2'>
           <img src='/img/story.svg' alt='story' />
@@ -76,27 +73,24 @@ function App() {
 
       {/*   my michis machas */}
       <section className='mt-24 md:px-24 px-8 '>
-        <h1 className='text-7xl text-custom-yellow font-bubblegum text-stroke-bold text-center'>My Michis Machas</h1>
+        <h1 className='text-7xl text-custom-yellow font-bubblegum text-stroke-bold text-center'>{t('my_michis_machas')}</h1>
         <div className='flex flex-col md:flex-row'>
           <div className='w-full'>
             <img src='/img/cute.svg' alt='cute' />
             <h1 className='text-custom-red text-stroke font-bubblegum text-4xl'>Cute</h1>
-            <p className='text-custom-gray text-2xl font-bubblegumSans'>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+            <p className='text-custom-gray text-2xl font-bubblegumSans'>{t('my_michis_machas_desc')}
             </p>
           </div>
           <div className='w-full'>
             <img src='/img/cuter.svg' alt='cute' />
             <h1 className='text-custom-red text-stroke font-bubblegum text-4xl'>Cuter</h1>
-            <p className='text-custom-gray text-2xl font-bubblegumSans'>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+            <p className='text-custom-gray text-2xl font-bubblegumSans'>{t('my_michis_machas_desc')}
             </p>
           </div>
           <div className='w-full'>
             <img src='/img/cutest.svg' alt='cute' className='w-full' />
             <h1 className='text-custom-red text-stroke font-bubblegum text-4xl'>Cutest</h1>
-            <p className='text-custom-gray text-2xl font-bubblegumSans'>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+            <p className='text-custom-gray text-2xl font-bubblegumSans'>{t('my_michis_machas_desc')}
             </p>
           </div>
         </div>
@@ -112,12 +106,12 @@ function App() {
             <img src='/img/utility-3.svg' alt='utility 3' />
           </div>
           <div className='w-full md:w-1/2 p-12 md:p-4'>
-            <h1 className='text-custom-red text-stroke font-bubblegum text-4xl'>Include</h1>
+            <h1 className='text-custom-red text-stroke font-bubblegum text-4xl'>{t('include')}</h1>
             <ul className='text-custom-gray text-2xl font-bubblegumSans'>
-              <li className='list-disc'>amco laboris nisi ut aliquip ex ea commodo consequat. Duis aute laboris nisi ut  commodo consequat. </li>
-              <li className='list-disc'>amco laboris nisi ut aliquip ex ea commodo consequat. Duis aute laboris nisi ut  commodo consequat. </li>
-              <li className='list-disc'>amco laboris nisi ut aliquip ex ea commodo consequat. Duis aute laboris nisi ut  commodo consequat. </li>
-              <li className='list-disc'>amco laboris nisi ut aliquip ex ea commodo consequat. Duis aute laboris nisi ut  commodo consequat. </li>
+              <li className='list-disc'>{t('include_desc')}</li>
+              <li className='list-disc'>{t('include_desc')}</li>
+              <li className='list-disc'>{t('include_desc')}</li>
+              <li className='list-disc'>{t('include_desc')}</li>
             </ul>
           </div>
         </div>
@@ -131,12 +125,12 @@ function App() {
             <div className='border-2 rounded-lg border-custom-red'>
               <div className='w-full border-2 border-custom-red rounded-lg p-8 bg-gradient-to-b from-custom-red -rotate-6'>
                 <div className='rotate-6'>
-                  <h1 className='text-custom-blue text-stroke font-bubblegum text-center text-4xl'>Phase 1</h1>
+                  <h1 className='text-custom-blue text-stroke font-bubblegum text-center text-4xl'>{t('phase')} 1</h1>
                   <ul className='text-custom-gray text-2xl font-bubblegumSans'>
-                    <li className='list-disc'>amco laboris nisi ut aliquip ex ea. Duis aute laboris nisi ut  commodo consequat. </li>
-                    <li className='list-disc'>amco laboris nisi ut aliquip ex ea. Duis aute laboris nisi ut  commodo consequat. </li>
-                    <li className='list-disc'>amco laboris nisi ut aliquip ex ea. Duis aute laboris nisi ut  commodo consequat. </li>
-                    <li className='list-disc'>amco laboris nisi ut aliquip ex ea. Duis aute laboris nisi ut  commodo consequat. </li>
+                    <li className='list-disc'>{t('phase_desc')}</li>
+                    <li className='list-disc'>{t('phase_desc')}</li>
+                    <li className='list-disc'>{t('phase_desc')}</li>
+                    <li className='list-disc'>{t('phase_desc')}</li>
                   </ul>
                 </div>
               </div>
@@ -146,12 +140,12 @@ function App() {
             <div className='border-2 rounded-lg border-custom-red'>
               <div className='w-full border-2 border-custom-red rounded-lg p-8 bg-gradient-to-b from-custom-red -rotate-6'>
                 <div className='rotate-6'>
-                  <h1 className='text-custom-blue text-stroke font-bubblegum text-center text-4xl'>Phase 2</h1>
+                  <h1 className='text-custom-blue text-stroke font-bubblegum text-center text-4xl'>{t('phase')} 2</h1>
                   <ul className='text-custom-gray text-2xl font-bubblegumSans'>
-                    <li className='list-disc'>amco laboris nisi ut aliquip ex ea. Duis aute laboris nisi ut  commodo consequat. </li>
-                    <li className='list-disc'>amco laboris nisi ut aliquip ex ea. Duis aute laboris nisi ut  commodo consequat. </li>
-                    <li className='list-disc'>amco laboris nisi ut aliquip ex ea. Duis aute laboris nisi ut  commodo consequat. </li>
-                    <li className='list-disc'>amco laboris nisi ut aliquip ex ea. Duis aute laboris nisi ut  commodo consequat. </li>
+                    <li className='list-disc'>{t('phase_desc')}</li>
+                    <li className='list-disc'>{t('phase_desc')}</li>
+                    <li className='list-disc'>{t('phase_desc')}</li>
+                    <li className='list-disc'>{t('phase_desc')}</li>
                   </ul>
                 </div>
               </div>
@@ -161,12 +155,12 @@ function App() {
             <div className='border-2 rounded-lg border-custom-red'>
               <div className='w-full border-2 border-custom-red rounded-lg p-8 bg-gradient-to-b from-custom-red -rotate-6'>
                 <div className='rotate-6'>
-                  <h1 className='text-custom-blue text-stroke font-bubblegum text-center text-4xl'>Phase 3</h1>
+                  <h1 className='text-custom-blue text-stroke font-bubblegum text-center text-4xl'>{t('phase')} 3</h1>
                   <ul className='text-custom-gray text-2xl font-bubblegumSans'>
-                    <li className='list-disc'>amco laboris nisi ut aliquip ex ea. Duis aute laboris nisi ut  commodo consequat. </li>
-                    <li className='list-disc'>amco laboris nisi ut aliquip ex ea. Duis aute laboris nisi ut  commodo consequat. </li>
-                    <li className='list-disc'>amco laboris nisi ut aliquip ex ea. Duis aute laboris nisi ut  commodo consequat. </li>
-                    <li className='list-disc'>amco laboris nisi ut aliquip ex ea. Duis aute laboris nisi ut  commodo consequat. </li>
+                    <li className='list-disc'>{t('phase_desc')}</li>
+                    <li className='list-disc'>{t('phase_desc')}</li>
+                    <li className='list-disc'>{t('phase_desc')}</li>
+                    <li className='list-disc'>{t('phase_desc')}</li>
                   </ul>
                 </div>
               </div>
@@ -178,7 +172,7 @@ function App() {
 
       {/* Team */}
       <section className='mt-24 md:px-24 px-8 '>
-        <h1 className='text-7xl text-custom-yellow font-bubblegum text-stroke-bold text-center py-4'>Team</h1>
+        <h1 className='text-7xl text-custom-yellow font-bubblegum text-stroke-bold text-center py-4'>{t('team')}</h1>
         <div className='flex flex-col md:-px-24 md:flex-row'>
           <div className='w-full'>
             <img src='/img/team.svg' alt='team' />
@@ -187,27 +181,27 @@ function App() {
             <div className='flex my-2 gap-4'>
               <img src='/icons/linkedin.svg' alt='linkedin' />
               <p className='text-stroke font-bubblegum text-custom-blue text-3xl'>
-                <span className='text-custom-red '>Cuter,</span> THE MOST CUTE MAN YOU'VE EVER HEARD</p>
+                <span className='text-custom-red '>Cuter,</span> {t('team_desc')}</p>
             </div>
             <div className='flex my-2 gap-4'>
               <img src='/icons/linkedin.svg' alt='linkedin' />
               <p className='text-stroke font-bubblegum text-custom-blue text-3xl'>
-                <span className='text-custom-red '>Cuter2,</span> THE MOST CUTE MAN YOU'VE EVER HEARD</p>
+                <span className='text-custom-red '>Cuter2,</span> {t('team_desc')}</p>
             </div>
             <div className='flex my-2 gap-4'>
               <img src='/icons/linkedin.svg' alt='linkedin' />
               <p className='text-stroke font-bubblegum text-custom-blue text-3xl'>
-                <span className='text-custom-red '>Cuter3,</span> THE MOST CUTE MAN YOU'VE EVER HEARD</p>
+                <span className='text-custom-red '>Cuter3,</span> {t('team_desc')}</p>
             </div>
             <div className='flex my-2 gap-4'>
               <img src='/icons/linkedin.svg' alt='linkedin' />
               <p className='text-stroke font-bubblegum text-custom-blue text-3xl'>
-                <span className='text-custom-red '>Cuter4,</span> THE MOST CUTE MAN YOU'VE EVER HEARD</p>
+                <span className='text-custom-red '>Cuter4,</span> {t('team_desc')}</p>
             </div>
             <div className='flex my-2 gap-4'>
               <img src='/icons/linkedin.svg' alt='linkedin' />
               <p className='text-stroke font-bubblegum text-custom-blue text-3xl'>
-                <span className='text-custom-red '>Cuter5,</span> THE MOST CUTE MAN YOU'VE EVER HEARD</p>
+                <span className='text-custom-red '>Cuter5,</span> {t('team_desc')}</p>
             </div>
           </div>
         </div>
@@ -235,7 +229,7 @@ function App() {
               <img src='icons/instagram-solid.svg' alt='icon instagram' />
             </a>
           </div>
-          <p className='text-center font-bold font-bubblegumSans mt-2'>@ 2022 Michi Macha | Privacy Policy | Terms and Conditions </p>
+          <p className='text-center font-bold font-bubblegumSans mt-2'>@ 2022 Michi Macha | {t('privacy')} | {t('tnc')} </p>
         </div>
       </section>
     </div >
